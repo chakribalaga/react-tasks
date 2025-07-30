9144deae9923ba5d368cd01e727f0ebe535e113a
Balaga Chakradhar rao <chakribalaga1707@gmail.com>
2025-07-30 15:27:33 +0530

    profile conpleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Pages/LoginPages/Login';
 import Signup from './Pages/SignupPages/Signup';
-
+import Profile from './Pages/Profile/Profile';
 function App() {
   return (
-    <Router>
+    <>
+    {/* <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
-    </Router>
+    </Router> */}
+    <Profile/>
+    </>
   );
 }
 
---
+++ src/Pages/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Profile.css";
+
+function Profile(){
+return(
+    <div className="pro-main-cont">
+        <div className="pro-first-cont">
+            <h1 className="pro-heading">Employ profile</h1>
+        </div>
+        <div className="pro-second-cont">
+            <div className="profile-img-cont">
+                <img className="profile-img" src="/assets/profile-img.png" alt="Profile" />
+            </div>
+            <div className="inf-cont">
+                <h1 className="emp-name-heading">
+                    Balaga Chakradhar Rao
+                 </h1>
+                 <h2 className="emp-email-head">
+                    Email :   <span>
+                          Chakribalaga56@gmail.com
+                    </span>
+                 </h2>
+                 <h2 className="emp-email-head">
+                    Phone :      
+                    <span>
+                          1234567980
+                    </span>
+                 </h2>
+                 <button className="pro-logout-btn">Logout</button>
+            </div>
+        </div>
+    </div>
+)
+    
+}
+export default Profile;
