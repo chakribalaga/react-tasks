32e78dccbd44fc4c87c14bc78f6946ab4513593a
Balaga Chakradhar Rao <chakribalaga1707@gmail.com>
2025-07-31 21:27:47 +0530

    modified the local code

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Profile from './Pages/Profile/Profile';
 function App() {
   return (
     <>
-    <Router>
+    {/* <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
-    </Router>
-    {/* <Profile/> */}
+    </Router> */}
+    <Profile/>
     </>
   );
 }
